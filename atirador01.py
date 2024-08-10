c27f11036b3bc8e1079155ad381ec61870675560
Moisés Moreira <mastermoreira2002@hotmail.com>
2024-08-10 17:26:10 -0300

    Update atirador01.py

--- atirador01.py
+++ atirador01.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 
@@ -11,41 +12,50 @@ pygame.display.set_caption('Atirador')
 # Taxa de frames
 clock = pygame.time.Clock()
 FPS = 60
+
+# Definir variáveis do jogo
+GRAVITY = 0.75
 # Ação do jogador
 moving_left = False
 moving_right = False
 
 # Definir cores
 BG = (144, 201, 120)
+RED = (255, 0, 0)
 
 def draw_bg():
     screen.fill(BG)
-    
+    pygame.draw.line(screen, RED, (0, 300), (SCREEN_WIDTH, 300))
 
 class Soldado(pygame.sprite.Sprite):
     def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
+        self.alive = True
         self.char_type = char_type
         self.speed = speed
         self.direction = 1
+        self.vel_y = 0
+        self.jump = False
+        self.in_air = True
         self.flip = False
         self.animation_list = []
         self.frame_index = 0
         self.action = 0
         self.update_time = pygame.time.get_ticks()
-        temp_list = []
-        for i in range(5):
-            img = pygame.image.load(f'Jogo/img/{self.char_type}/Idle/{i}.png')
-            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-            temp_list.append(img)
-        self.animation_list.append(temp_list)    
-            
-        temp_list = []
-        for i in range(6):
-            img = pygame.image.load(f'Jogo/img/{self.char_type}/Run/{i}.png')
-            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-            temp_list.append(img)
-        self.animation_list.append(temp_list)
+        
+        # Carregar todas imagens do jogador
+        animation_types = ['Idle', 'Run', 'Jump']
+        for animation in animation_types:
+            # Redefinir lista temporária de imagens
+            temp_list = []
+            # Conta o número de arquivos na pasta
+            num_of_frames = len(os.listdir(f'Jogo/img/{self.char_type}/{animation}'))
+            for i in range(num_of_frames):
+                img = pygame.image.load(f'Jogo/img/{self.char_type}/{animation}/{i}.png')
+                img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+                temp_list.append(img)
+            self.animation_list.append(temp_list)    
+
         self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
@@ -64,6 +74,23 @@ class Soldado(pygame.sprite.Sprite):
             self.flip = False
             self.direction = 1
         
+        # pular
+        if self.jump == True and self.in_air == False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+        
+        # Aplicar gravidade
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+            self.vel_y
+        dy += self.vel_y
+        
+        # Verificar colisão com o piso
+        if self.rect.bottom + dy > 300:
+            dy = 300 - self.rect.bottom
+            self.in_air = False
+        
         self.rect.x += dx
         self.rect.y += dy
     
@@ -80,7 +107,14 @@ class Soldado(pygame.sprite.Sprite):
         if self.frame_index >= len(self.animation_list[self.action]):
             self.frame_index = 0        
         
-        
+
+    def update_action(self, new_action):
+        # A nova ação é diferenten da anterior 
+        if new_action != self.action:
+            self.action = new_action
+            # Atualizar as configurações de animação
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()     
     
     def desenhar(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
@@ -101,7 +135,16 @@ while run:
     player.desenhar()
     enemy.desenhar()
  
-    player.move(moving_left, moving_right)
+    
+    # Atualiza as ações do jogador
+    if player.alive:
+        if player.in_air:
+            player.update_action(2) #2: Pular
+        elif moving_left or moving_right:
+            player.update_action(1) #1: correr
+        else:
+            player.update_action(0) #0: ocioso
+        player.move(moving_left, moving_right)
     
     for event in pygame.event.get():
         # sair game
@@ -113,6 +156,8 @@ while run:
                 moving_left = True
             if event.key == pygame.K_d:
                 moving_right = True
+            if event.key == pygame.K_w and player.alive:
+                player.jump = True
             if event.key == pygame.K_ESCAPE:
                 run = False
         
